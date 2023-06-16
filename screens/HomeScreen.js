ad3dec29a4a2818236597fd52223682596d3c3b1
JustZion <olamigokezee@gmail.com>
2023-06-16 11:22:36 +0100

    initialized Googlemaps autocomplete

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,8 @@ import { View, Text, SafeAreaView, Image } from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames'
 import NavOptions from '../components/NavOptions'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from '@env'
 
 const HomeScreen = () => {
   return (
@@ -16,6 +18,32 @@ const HomeScreen = () => {
             source={
             require('../images/uber.jpg')
             } 
+        />
+
+        <GooglePlacesAutocomplete
+            styles= {{
+                container: {
+                    flex: 0
+                },
+                textInput: {
+                    fontSize: 18
+                }
+            }}
+
+            query= {{
+                key: GOOGLE_MAPS_APIKEY,
+                language: 'en',
+            }}
+            onPress={(data, details = null) => {
+                console.log(data, details)
+            }}
+
+            fetchDetails={true}
+            minLength={2}
+            enablePoweredByContainer={false}
+            nearbyPlacesAPI='GooglePlacesSearch'
+            placeholder='Where from ?'
+            debounce={100}
         />
 
         <NavOptions />
