94651be21acb39d673d69c2a57567503573a2a2d
JustZion <olamigokezee@gmail.com>
2023-06-23 10:48:31 +0100

    some changes

--- components/RideCarsPark.js
+++ components/RideCarsPark.js
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
+import { View, Text, SafeAreaView, TouchableOpacity, FlatList , Image} from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames'
 import { Icon } from 'react-native-elements'
@@ -12,13 +12,13 @@ const data = [{
   image: require('../images/regular.png'),
 },
 {
-  id: '1',
+  id: '2',
   title: 'Premium',
   multiplier: 1.25,
   image: require('../images/premium.png'),
 },
 {
-  id: '1',
+  id: '3',
   title: 'Special',
   multiplier: 1.75,
   image: require('../images/special.png'),
@@ -35,10 +35,24 @@ const RideCarsPack = () => {
           onPress={() => Navigate.navigate('NavigateCard')}>
           <Icon name='chevron-left' type='font-awesome' />
         </TouchableOpacity>
-        <Text style={tw`text-xl ml-auto mr-auto`}>Select a Ride</Text>
+        <Text style={tw`text-xl ml-auto mr-auto font-semibold`}>Select a Ride</Text>
         </View>
 
-        
+        <FlatList style={tw`mt-2`} data={data} keyExtractor={(item) => item.id}
+        renderItem={({item : {id, title, multiplier, image}}) => (
+          <TouchableOpacity style={tw`flex-row text-center justify-between px-5 pl-2`}>
+            <Image 
+             source= {image}
+             style= {{ width: 100, height: 100 , resizeMode: 'contain' }}
+            />
+            <View style={tw`mt-7 -ml-2`}>
+              <Text>{title}</Text>
+              <Text>Multiplier of ...</Text>
+            </View>
+            <Text style={tw`font-semibold text-xl mt-7`}>£99</Text>
+          </TouchableOpacity>
+        )}
+        />
     </SafeAreaView>
    
   )
